89294f999480c1375440e5e281036fcb94e41dd6
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-12 00:38:44 +0530

    update: database and index

--- database.js
+++ database.js
@@ -1,23 +1,23 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// const connection = mysql.createConnection({
-// 	host: process.env.host,
-//     user: process.env.user,
-//     password: process.env.password,
-//     database: process.env.database,
-// });
+const connection = mysql.createConnection({
+	host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+});
 
-// connection.connect(function(error){
-// 	if(error)
-// 	{
-// 		console.error('error connecting: ' + error.stack);
-//         return;
-// 	}
-// 	else
-// 	{
-// 		console.log('connected as id ' + connection.threadId);
-// 	}
-// });
+connection.connect(function(error){
+	if(error)
+	{
+		console.error('error connecting: ' + error.stack);
+        return;
+	}
+	else
+	{
+		console.log('connected as id ' + connection.threadId);
+	}
+});
 
-// module.exports = connection;
+module.exports = connection;
--- routes/index.js
+++ routes/index.js
@@ -12,31 +12,29 @@ router.use(
     }),
   );
 
-router.post('/login', bodyParser.json(),function(request, response) {
+router.post('/login',function(request, response) {
 	// Capture the input fields
 	let username = request.body.username;
 	let password = request.body.password;
-    console.log(username)
+   
 	// Ensure the input fields exists and are not empty
 	if (username && password) {
-        console.log("x")
+        
 		// Execute SQL query that'll select the account from the database based on the specified username and password
 		// database.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
 		// 	// If there is an issue with the query, output the error
 		// 	if (error) throw error;
 		// 	// If the account exists
 		// 	if (results.length > 0) {
-		// 		// Authenticate the user
-		// 		request.body.loggedin = true;
-		// 		request.body.username = username;
+		// 		
 		// 		// Redirect to home page
-		// 		response.redirect('/');
+		 		response.send(username);
 		// 	} else {
 		// 		response.send('Incorrect Username and/or Password!');
 		// 	}			
 		// 	response.end();
 		// });
-		response.send('hey');
+		
 	} else {
 		response.send('Please enter Username and Password!');
 		response.end();
