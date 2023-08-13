3f3fd3e610a39a1ff235d71b4a5be6495141d656
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-13 17:13:13 +0530

    comments removed

--- routes/index.js
+++ routes/index.js
@@ -20,19 +20,19 @@ router.post("/login", function (request, response) {
   // Ensure the input fields exists and are not empty
   if (username && password) {
     // Execute SQL query that'll select the account from the database based on the specified username and password
-    // database.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-    // 	// If there is an issue with the query, output the error
-    // 	if (error) throw error;
-    // 	// If the account exists
-    // 	if (results.length > 0) {
-    //
-    // 		// Redirect to home page
-    response.send(username);
-    // 	} else {
-    // 		response.send('Incorrect Username and/or Password!');
-    // 	}
-    // 	response.end();
-    // });
+    database.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+    	// If there is an issue with the query, output the error
+    	if (error) throw error;
+    	// If the account exists
+    	if (results.length > 0) {
+    
+    		// Redirect to home page
+            response.send(username);
+    	} else {
+    		response.send('Incorrect Username and/or Password!');
+    	}
+    	response.end();
+    });
   } else {
     response.send("Please enter Username and Password!");
     response.end();
