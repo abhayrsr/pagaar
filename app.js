c3de6e3a6c6c327d5a98d1f91830d648ab585963
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-15 14:52:35 +0530

    update: usersearch router

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const port = 3000;
 var indexRouter = require("./routes/index");
 var walletRouter = require("./routes/wallet");
 var transactionRouter = require("./routes/transaction");
+var userSearchRouter = require("./routes/userSearch");
 
 app.use("/users", indexRouter);
 app.use("/users", walletRouter);
 app.use("/users", transactionRouter);
+app.use("/users", userSearchRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
