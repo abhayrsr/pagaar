9bad420fc3c08333744c4c4000e4c310617c0845
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-15 13:05:04 +0530

    update: import walletRouter

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express();
 const port = 3000;
 
 var indexRouter = require("./routes/index");
+var walletRouter = require("./routes/wallet");
 
 app.use("/users", indexRouter);
+app.use("/users", walletRouter);
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
