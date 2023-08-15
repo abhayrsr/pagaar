e3fee130fb7afed3dbfab4c3b3431781dfb18300
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-15 19:06:37 +0530

    update: sendMoney router

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ var indexRouter = require("./routes/index");
 var walletRouter = require("./routes/wallet");
 var transactionRouter = require("./routes/transaction");
 var userSearchRouter = require("./routes/userSearch");
+var sendMoneyRouter = require("./routes/sendMoney");
 
 app.use("/users", indexRouter);
 app.use("/users", walletRouter);
 app.use("/users", transactionRouter);
 app.use("/users", userSearchRouter);
+app.use("/users", sendMoneyRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
