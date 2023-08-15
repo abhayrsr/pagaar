7edd2ff0ae2f46633d9f5e577ee6fafb6cc3132f
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-15 13:11:17 +0530

    feat: wallet router

---
+++ routes/wallet.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express();
+var database = require("../database");
+const bodyParser = require("body-parser");
+
+
+router.get('/wallet', function(request, response){
+    const walletId = request.query.wallet_id;
+
+    if(walletId){
+        database.query(
+            `select wallet_id, balance from wallet where wallet_id = ?`, [walletId], 
+            function(error, results){
+              try { 
+                
+                if(results.length > 0){
+                    console.log(results);
+                    return response.status(200).json({data: results})
+                } else {
+                    return response.status(400).json({error: "Incorrect wallet_id"})
+                }
+            } catch(error) {
+                return response.status(500).json({error: "Internal server error"})
+            }
+            }
+    )} else {
+        return response.status(401).json({error: "Enter wallet_id"})
+    }
+    
+})
+
+module.exports = router;
