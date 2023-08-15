89b975d4b33c660fba73e16af7a0db20b165049a
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-15 13:07:07 +0530

    refactor: comments

--- routes/index.js
+++ routes/index.js
@@ -29,9 +29,11 @@ router.post("/login", async function (request, response) {
 
         try {
           // If the account exists
+          
           if (results.length > 0) {
-            // Redirect to home page
+
             return response.status(200).json({ username: username });
+
           } else {
             // response.send('Incorrect Username and/or Password!');
             return response
