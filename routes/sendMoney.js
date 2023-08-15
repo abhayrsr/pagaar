c174ea8f1e3fee09559e6d5d10d4364c57f0fabe
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-15 23:10:40 +0530

    feat: send money

--- routes/sendMoney.js
+++ routes/sendMoney.js
@@ -15,49 +15,55 @@ router.use(
 router.post("/sendmoney", async function (request, response) {
   const data = await request.body;
 
-  if (data.sender_id && data.receiver_id) {
+  if (data.sender_id && data.receiver_id && data.amount) {
     database.query(
       "select balance from wallet where user_id = ?",
       [data.sender_id],
       function (err, results) {
-        var balance = results[0].balance;
+        try {
+          var balance = results[0].balance;
+        } catch (error) {
+          return response.send("invalid sender id");
+        }
 
-        database.query(
-          "select transaction_id, sender_id, receiver_id, transaction_date, status from transactions where sender_id = ? and receiver_id = ?",
-          [data.sender_id, data.receiver_id],
-          function (error, results) {
-            try {
-              if (results.length > 0) {
-                if (data.amount < balance) {
-                  database.query(
-                    `update wallet set balance = ${balance} - ${data.amount} where user_id = ?`,
-                    [data.sender_id]
-                  );
-                  database.query(
-                    `update wallet set balance = ${balance} + ${data.amount} where user_id = ?`,
-                    [data.receiver_id]
-                  );
-                  database.query(
-                    `insert into transactions(sender_id, receiver_id, status, amount) values (?, ?, ?, ?)`,
-                    [data.sender_id, data.receiver_id, "sent", data.amount]
-                  );
+        if (data.amount < balance) {
+          database.query(
+            `update transactions set amount = ${data.amount} where sender_id = ? and receiver_id = ?`,
+            [data.sender_id, data.receiver_id]
+          );
+          database.query(
+            `update wallet set balance = ${balance} - ${data.amount} where user_id = ?`,
+            [data.sender_id]
+          );
+          database.query(
+            `update wallet set balance = ${balance} + ${data.amount} where user_id = ?`,
+            [data.receiver_id]
+          );
+          database.query(
+            `insert into transactions(sender_id, receiver_id, status, amount) values (?, ?, ?, ?)`,
+            [data.sender_id, data.receiver_id, "sent", data.amount]
+          );
 
+          database.query(
+            `select transactions.transaction_id, transactions.sender_id, transactions.receiver_id, transactions.transaction_date, transactions.status, transactions.amount, wallet.balance from transactions inner join wallet on transactions.sender_id = wallet.user_id where sender_id = ${data.sender_id}`,
+
+            function (error, results) {
+              try {
+                if (results.length > 0) {
                   return response.status(200).json({ data: results });
-                } else {
-                  return response.send("Insufficient balance");
                 }
-              } else {
+              } catch (error) {
                 return response
                   .status(400)
                   .json({ error: "Incorrect details" });
               }
-            } catch (error) {
-              return response
-                .status(500)
-                .json({ error: "Internal server error" });
             }
-          }
-        );
+          );
+        } else if (data.amount > balance) {
+          return response.send("Insufficient balance");
+        } else {
+          return response.status(500).json({ error: "Internal server error" });
+        }
       }
     );
   } else {
