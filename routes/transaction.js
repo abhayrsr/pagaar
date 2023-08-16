62d6a49cd06ee8824cb2a4bfb6bc97dde37cc182
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-16 23:22:51 +0530

    update: used promise wrapper

--- routes/transaction.js
+++ routes/transaction.js
@@ -2,28 +2,29 @@ var express = require("express");
 var router = express();
 var database = require("../database");
 
-router.get("/transactions", function (request, response) {
-  const userId = request.query.user_id;
+try {
+  router.get("/transactions", async function (request, response) {
+    const userId = request.query.user_id;
+    const query = `select transaction_id, sender_id, receiver_id, status,transaction_date, amount from transactions where receiver_id = ? or sender_id = ?`;
 
-  if (userId) {
-    database.query(
-      `select transaction_id, sender_id, receiver_id, status,transaction_date, amount from transactions where receiver_id = ? or sender_id = ?`,
-      [userId, userId],
-      function (error, results) {
-        try {
-          if (results.length > 0) {
-            return response.status(200).json({ data: results });
-          } else {
-            return response.status(400).json({ error: "Incorrect user_id" });
-          }
-        } catch (error) {
-          return response.status(500).json({ error: "Internal server error" });
+    if (userId) {
+      try {
+        const [rows, fields] = await database.query(query, [userId, userId]);
+
+        if (rows.length > 0) {
+          return response.status(200).json({ data: rows });
+        } else {
+          return response.status(400).json({ error: "Incorrect user_id" });
         }
+      } catch (error) {
+        return response.status(500).json({ error: "Internal server error" });
       }
-    );
-  } else {
-    return response.status(401).json({ error: "Enter user_id" });
-  }
-});
+    } else {
+      return response.status(401).json({ error: "Enter user_id" });
+    }
+  });
+} catch (error) {
+  return response.status(500).json({ error: "Internal server error" });
+}
 
 module.exports = router;
