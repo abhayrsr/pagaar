8dfb37b33a2ec5b4f040e9a226b7302a67304168
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-16 23:08:12 +0530

    update: used promise wrapper

--- routes/wallet.js
+++ routes/wallet.js
@@ -2,28 +2,29 @@ var express = require("express");
 var router = express();
 var database = require("../database");
 
-router.get("/wallet", function (request, response) {
-  const walletId = request.query.wallet_id;
+try {
+  router.get("/wallet", async function (request, response) {
+    const walletId = request.query.wallet_id;
+    const query = `select wallet_id, balance from wallet where wallet_id = ?`;
 
-  if (walletId) {
-    database.query(
-      `select wallet_id, balance from wallet where wallet_id = ?`,
-      [walletId],
-      function (error, results) {
-        try {
-          if (results.length > 0) {
-            return response.status(200).json({ data: results });
-          } else {
-            return response.status(400).json({ error: "Incorrect wallet_id" });
-          }
-        } catch (error) {
-          return response.status(500).json({ error: "Internal server error" });
+    if (walletId) {
+      try {
+        const [rows, fields] = await database.query(query, [walletId]);
+        if (rows.length > 0) {
+          return response.status(200).json({ data: rows });
+        } else {
+          return response.status(400).json({ error: "Incorrect wallet_id" });
         }
+      } catch (error) {
+        return response.status(500).json({ error: "Internal server error" });
       }
-    );
-  } else {
-    return response.status(401).json({ error: "Enter wallet_id" });
-  }
-});
+    } else {
+      return response.status(401).json({ error: "Enter wallet_id" });
+    }
+
+  });
+} catch (error) {
+  return response.status(500).json({ error: "Internal server error" });
+}
 
 module.exports = router;
