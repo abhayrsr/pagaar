0bc56626ad9fff6850b55d99d91a1400ff669ea2
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-08-18 19:48:01 +0530

    update: status-error

--- routes/wallet.js
+++ routes/wallet.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express();
 var database = require("../database");
+var status = require("http-status")
 
 try {
   router.get("/wallet", async function (request, response) {
@@ -11,20 +12,20 @@ try {
       try {
         const [rows, fields] = await database.query(query, [walletId]);
         if (rows.length > 0) {
-          return response.status(200).json({ data: rows });
+          return response.status(status.OK).json({ data: rows });
         } else {
-          return response.status(400).json({ error: "Incorrect wallet_id" });
+          return response.status(status.BAD_REQUEST).json({ error: "Incorrect wallet_id" });
         }
       } catch (error) {
-        return response.status(500).json({ error: "Internal server error" });
+        return response.status(status.INTERNAL_SERVER_ERROR).json({ error: "Internal server error" });
       }
     } else {
-      return response.status(401).json({ error: "Enter wallet_id" });
+      return response.status(status.UNAUTHORIZED).json({ error: "Enter wallet_id" });
     }
 
   });
 } catch (error) {
-  return response.status(500).json({ error: "Internal server error" });
+  return response.status(status.INTERNAL_SERVER_ERROR).json({ error: "Internal server error" });
 }
 
 module.exports = router;
